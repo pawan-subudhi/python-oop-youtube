2cea7c0ad078f4dbcc980e28d47ff6566f7b9088
Pawan Kumar <37031863+pawan-subudhi@users.noreply.github.com>
2024-01-08 05:12:18 +0000

    Basic setup for store management

---
+++ store_management_system/main.py
@@ -0,0 +1,57 @@
+import csv
+
+
+class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
+    # the self indicates that python implicitly passes the instance of class as first parameter, like in class method the class reference is passed implicitly
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            # the DictReader will convert the file data read to dict format
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+    
+Item.instantiate_from_csv()
+print(Item.all)
